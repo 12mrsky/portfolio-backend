5f1e1e799070794f682c273464a7cc8ab7a9e146
Suraj <surajyadu.9@gmail.com>
2026-03-28 01:03:42 +0530

    Downgraded project to .NET 8

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a4f3b88f1d479574a0be0528d1104ef8f47f02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fedb84d35874c713b9ec8eb058807c3bba5dab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
